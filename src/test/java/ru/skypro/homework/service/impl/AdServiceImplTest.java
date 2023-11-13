77274ec2c84f75e133acf5ad7c5a273142f69625
microd <microd@inbox.ru>
2023-11-13 20:30:31 +0500

    #12.1 пробное тестирование

--- src/test/java/ru/skypro/homework/service/impl/AdServiceImplTest.java
+++ src/test/java/ru/skypro/homework/service/impl/AdServiceImplTest.java
@@ -2,17 +2,21 @@ package ru.skypro.homework.service.impl;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import ru.skypro.homework.dto.AdDto;
 import ru.skypro.homework.dto.AdsDto;
+import ru.skypro.homework.dto.CreateOrUpdateAdDto;
 import ru.skypro.homework.entity.*;
 import ru.skypro.homework.enums.Role;
 import ru.skypro.homework.mapper.AdMapper;
 import ru.skypro.homework.repository.AdRepository;
 import ru.skypro.homework.service.AdService;
 import ru.skypro.homework.service.ImageService;
+import ru.skypro.homework.utils.AuthUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +36,8 @@ class AdServiceImplTest {
     private AdRepository adRepository;
     @Mock
     private AdMapper adMapper;
+    @Mock
+    private AuthUtils authUtils;
 
     @Test
     public void test1() {
@@ -65,29 +71,17 @@ class AdServiceImplTest {
 
     @Test
     public void test3() {
-        AdDto adDto1 = new AdDto(1, "image", 1, 100, "title title title");
-        AdDto adDto2 = new AdDto(2, "image2", 2, 200, "title2 title2 title2");
-
-        UserImage userImage = new UserImage();
+        User user = new User("login", "password", "firstName", "lastname", "7999999999", Role.USER, null, null, null);
         AdImage adImage = new AdImage();
-        List<Ad> listAds = new ArrayList<>();
-        List<Comment> listComments = new ArrayList<>();
-        List<AdDto> adDtoList = new ArrayList<>(List.of(adDto1, adDto2));
+        when(authUtils.getUserFromAuthentication(any())).thenReturn(user);
+        when(imageService.updateImage(any(), any())).thenReturn(adImage);
+        CreateOrUpdateAdDto createOrUpdateAdDto = new CreateOrUpdateAdDto("title", 123, "description");
+        Ad ad = new Ad(123, "title", "description", user, adImage, null);
+        when(adMapper.createAdDtoToAd(any(), any(), any())).thenReturn(ad);
+        AdDto adDto = new AdDto(1, "image", 1, 123,"title title title");
+        when(adMapper.toAdDto(any())).thenReturn(adDto);
 
-        User user = new User("login", "password", "firstName", "lastName", "7999999999", Role.USER, userImage, listAds, listComments);
-
-        Ad ad1 = new Ad(100, "title", "description", user, adImage, listComments);
-        Ad ad2 = new Ad(200, "title2", "description2", user, adImage, listComments);
-
-        listAds.add(ad1);
-        listAds.add(ad2);
-
-
-        //ArrayList<AdDto> list = new ArrayList<>(List.of(adDto1, adDto2));
-
-        when(adRepository.findAll()).thenReturn(listAds);
-        AdsDto adsDto = new AdsDto(2, adDtoList);
-        assertEquals(adsDto, adService.getAllAds());
+        assertEquals(adDto, adService.addAd(any(), any()));
     }
 
 
