56b0118951f443aa81a60e4a577526cb2f32b950
Elena Koroleva <koroliana@yandex.ru>
2023-12-06 23:34:41 +0700

    В UserController протестирован метод setPassword() и частично getUser()

--- src/test/java/ru/skypro/homework/controller/UserControllerTest.java
+++ src/test/java/ru/skypro/homework/controller/UserControllerTest.java
@@ -1,29 +1,28 @@
 package ru.skypro.homework.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.skypro.homework.TestContainerInitializer;
 import ru.skypro.homework.dto.NewPasswordDto;
-import ru.skypro.homework.dto.RegisterDto;
+import ru.skypro.homework.dto.UserDto;
+import ru.skypro.homework.mapper.UserMapper;
 import ru.skypro.homework.repository.UserRepository;
 
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class UserControllerTest extends TestContainerInitializer {
 
@@ -32,6 +31,9 @@ public class UserControllerTest extends TestContainerInitializer {
 
     @Autowired
     private PasswordEncoder encoder;
+
+    @Autowired
+    private UserMapper userMapper;
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
@@ -55,9 +57,9 @@ public class UserControllerTest extends TestContainerInitializer {
         assertThat(userRepository.findByLogin(currentEmail))
                 .isPresent()
                 .get()
-                .satisfies(currentUser -> {
-                    assertTrue(encoder.matches(currentPassword, currentUser.getPassword()));
-                });
+                .satisfies(currentUser ->
+                    assertTrue(encoder.matches(currentPassword, currentUser.getPassword()))
+                );
 
         MockHttpServletResponse responsePost = mockMvc
                 .perform(MockMvcRequestBuilders
@@ -76,47 +78,98 @@ public class UserControllerTest extends TestContainerInitializer {
         assertThat(userRepository.findByLogin(currentEmail))
                 .isPresent()
                 .get()
-                .satisfies(currentUser -> {
-                    assertTrue(encoder.matches(password, currentUser.getPassword()));
-                });
+                .satisfies(currentUser ->
+                    assertTrue(encoder.matches(password, currentUser.getPassword()))
+                );
     }
 
     @Test
-    @DisplayName("Проверка обновления пароля, когда пользователь существует но пароль неверный")
+    @DisplayName("Проверка отклонения обновления пароля, когда логин верный, а пароль - нет")
     void givenNewPasswordDtoAndAuthorizedUserWithWrongPassword_whenChangePassword_thenReturnUnauthorized() throws Exception {
 
         NewPasswordDto newPasswordDto = new NewPasswordDto(password, password);
         String newPasswordDtoString = mapper.writeValueAsString(newPasswordDto);
 
-        // Проверка, что соответствующий пользователь есть в базе и у него исходный пароль
-        assertThat(userRepository.findByLogin(currentEmail))
+        // Проверка, что login пользователя, который пытается авторизоваться, есть в базе
+        assertTrue(userRepository
+                .findByLogin(currentEmail)
                 .isPresent()
-                .get()
-                .satisfies(currentUser -> {
-                    assertTrue(encoder.matches(currentPassword, currentUser.getPassword()));
-                });
+        );
 
         MockHttpServletResponse responsePost = mockMvc
                 .perform(MockMvcRequestBuilders
                         .post("/users/set_password")
                         .header(HttpHeaders.AUTHORIZATION,
                                 "Basic " + HttpHeaders.encodeBasicAuth(
-                                        currentEmail, currentPassword, StandardCharsets.UTF_8))
+                                        currentEmail, password, StandardCharsets.UTF_8))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(newPasswordDtoString))
                 .andReturn().getResponse();
 
-        // Проверка, что отдан успешный код ответа
-        assertThat(responsePost.getStatus()).isEqualTo(204);
+        // Проверка, что отдан 401 код ответа
+        assertThat(responsePost.getStatus()).isEqualTo(401);
 
-        // Проверка, что у пользователя в базе обновился пароль
+        // Проверка, что у пользователя в базе не обновился пароль
         assertThat(userRepository.findByLogin(currentEmail))
                 .isPresent()
                 .get()
-                .satisfies(currentUser -> {
-                    assertTrue(encoder.matches(password, currentUser.getPassword()));
-                });
+                .satisfies(currentUser ->
+                    assertFalse(encoder.matches(password, currentUser.getPassword()))
+                );
     }
 
+    @Test
+    @DisplayName("Проверка отклонения обновления пароля, когда пользователя не существует")
+    void givenNewPasswordDtoAndAuthorizedUserWhichIsNotExist_whenChangePassword_thenReturnUnauthorized() throws Exception {
+
+        NewPasswordDto newPasswordDto = new NewPasswordDto(currentPassword, password);
+        String newPasswordDtoString = mapper.writeValueAsString(newPasswordDto);
+
+        // Проверка, что пользователя, который пытается авторизоваться, нет в базе
+        assertFalse(userRepository
+                .findByLogin(email)
+                .isPresent()
+        );
+
+        MockHttpServletResponse responsePost = mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/users/set_password")
+                        .header(HttpHeaders.AUTHORIZATION,
+                                "Basic " + HttpHeaders.encodeBasicAuth(
+                                        email, currentPassword, StandardCharsets.UTF_8))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newPasswordDtoString))
+                .andReturn().getResponse();
+
+        // Проверка, что отдан 401 ответа
+        assertThat(responsePost.getStatus()).isEqualTo(401);
+    }
+
+    @Test
+    @DisplayName("Проверка получения информации об авторизованном пользователе")
+    void givenAuthorizedUser_whenGetAuthorizedUserDto_thenReturnOk() throws Exception {
+
+        //Получаем UserDto существующего пользователя, напрямую из базы
+        UserDto expectedUserDto = userMapper.toDto(
+                userRepository
+                        .findByLogin(currentEmail)
+                        .orElseThrow());
+
+        mockMvc.perform(
+                        get("/users/me")
+                                .header(HttpHeaders.AUTHORIZATION,
+                                        "Basic " + HttpHeaders.encodeBasicAuth(
+                                                currentEmail, currentPassword, StandardCharsets.UTF_8))
+                                .contentType(MediaType.APPLICATION_JSON))
+                //Проверяем, что отдан успешный код ответа и получены ожидаемые поля по авторизованному пользователю
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(expectedUserDto.id()))
+                .andExpect(jsonPath("$.email").value(expectedUserDto.email()))
+                .andExpect(jsonPath("$.firstName").value(expectedUserDto.firstName()))
+                .andExpect(jsonPath("$.lastName").value(expectedUserDto.lastName()))
+                .andExpect(jsonPath("$.phone").value(expectedUserDto.phone()))
+                .andExpect(jsonPath("$.role").value(expectedUserDto.role().toString()))
+                .andExpect(jsonPath("$.image").value(expectedUserDto.image()));
+    }
 
 }
